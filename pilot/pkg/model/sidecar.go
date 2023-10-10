1b9c6ca70053ed2f4ed0db0a35dbef9cbdaa2be7
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2023-10-10 11:21:44 -0300

    rename method

--- pilot/pkg/model/sidecar.go
+++ pilot/pkg/model/sidecar.go
@@ -281,17 +281,18 @@ func ConvertToSidecarScope(ps *PushContext, sidecarConfig *config.Config, config
 	}.HashCode())
 
 	if !features.EnableLazySidecarEvaluation {
-		initSidecarScope(ps, out, configNamespace)
+		initSidecarScopeInternalIndexes(ps, out, configNamespace)
 	} else {
 		out.initFunc = sync.OnceFunc(func() {
-			initSidecarScope(ps, out, configNamespace)
+			initSidecarScopeInternalIndexes(ps, out, configNamespace)
 		})
 	}
 
 	return out
 }
 
-func initSidecarScope(ps *PushContext, sidecarScope *SidecarScope, configNamespace string) {
+// initSidecarScopeInternalIndexes initializes SidecarScope's internal indexes derived from PushContext
+func initSidecarScopeInternalIndexes(ps *PushContext, sidecarScope *SidecarScope, configNamespace string) {
 	egressConfigs := sidecarScope.Sidecar.Egress
 	// If egress not set, setup a default listener
 	if len(egressConfigs) == 0 {
