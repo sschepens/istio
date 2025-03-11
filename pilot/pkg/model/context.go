99fda89541084c2755d66d476c951eb99ba81209
sschepens <sebastian.schepens@mercadolibre.com>
2025-03-11 15:31:06 -0300

    avoid updating service targets when it's not really necessary (#55476)

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -42,6 +42,7 @@ import (
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/config/mesh"
 	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/config/schema/kind"
 	"istio.io/istio/pkg/maps"
 	pm "istio.io/istio/pkg/model"
 	"istio.io/istio/pkg/monitoring"
@@ -548,6 +549,17 @@ func (node *Proxy) SetGatewaysForProxy(ps *PushContext) {
 		ContainsAutoPassthroughGateways: prevMergedGateway.ContainsAutoPassthroughGateways,
 		AutoPassthroughSNIHosts:         prevMergedGateway.GetAutoPassthroughGatewaySNIHosts(),
 	}
+}
+
+func (node *Proxy) ShouldUpdateServiceTargets(updates sets.Set[ConfigKey]) bool {
+	// we only care for services which can actually select this proxy
+	for config := range updates {
+		if config.Kind == kind.ServiceEntry || config.Namespace == node.Metadata.Namespace {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (node *Proxy) SetServiceTargets(serviceDiscovery ServiceDiscovery) {
